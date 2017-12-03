a630bc49d7c87686b4918e9840c214f5de334647
ikuchkarov <kuckarovildus@gmail.com>
2017-12-03 05:01:48 +0300

    add tags

--- train/train_tfidf.py
+++ train/train_tfidf.py
@@ -2,6 +2,7 @@ import os.path
 import nmslib
 import pickle
 from sklearn.decomposition import TruncatedSVD
+import scipy
 
 from deepoverflow.similar import build_tfidf_model
 
@@ -35,6 +36,24 @@ else:
 transformed_questions = tfidf.transform(cleaned_questions)
 
 print('transformed questions')
+
+tags_path = os.path.join(DATA_ROOT, 'incoming', 'Tags.csv')
+tags_df = pd.read_csv(tags_file,  encoding = "ISO-8859-1", index_col=2)
+top_tags = tags_df.sort_values(['Count'], ascending=False).TagName[:256]
+
+top_tags_path = os.path.join(DATA_ROOT, 'computed', 'top_tags.pickle')
+with open(top_tags_path, 'wb') as f:
+	pickle.dump(top_tags, f)
+	print('saved top_tags')
+
+additional_matrix = np.zeros((total, len(top_tags)), dtype='int64')
+temp_tags = posts_df[questions_mask]['Tags'].apply(lambda x: x.replace('<', '').replace('>', ' '))
+for i in range(len(top_tags)):
+    additional_matrix[:,i] = temp_tags.apply(lambda x: top_tags.iloc[i] in x).values
+additional_matrix = scipy.sparse.csr_matrix(additional_matrix, shape=additional_matrix.shape)
+transformed_questions = scipy.sparse.hstack(transformed_questions, scipy.sparse.csr_matrix(additional_matrix, shape=additional_matrix.shape)
+
+print('added tags')
 
 pca_path = os.path.join(DATA_ROOT, 'computed', 'pca.pickle')
 
